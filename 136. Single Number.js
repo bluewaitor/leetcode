0a82e95e5be4d5b83132f49f13032428b9324406
bluewaitor <405622394@qq.com>
2017-03-11 23:09:16 +0800

    136 speed up.

--- 136. Single Number.js
+++ 136. Single Number.js
@@ -10,9 +10,17 @@
  * @param {number[]} nums
  * @return {number}
  */
+// var singleNumber = function(nums) {
+//     //与或运算符的妙用(两个相同的数与或为0)
+//     return nums.reduce(function(acc, val){ return acc ^ val}, 0);
+// };
+
+//reduce比较慢
 var singleNumber = function(nums) {
-    //与或运算符的妙用(两个相同的数与或为0)
-    return nums.reduce(function(acc, val){ return acc ^ val}, 0);
+    for(var i = 1; i < nums.length; i++) {
+        nums[0] ^= nums[i];
+    }
+    return nums[0];
 };
 
 console.log(singleNumber([1,1,2,3,3]));
